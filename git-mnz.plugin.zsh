79eb404244e8cece7faf7cba9606e7eb027ce655
Massimo Rangoni <maxappbr@gmail.com>
2021-05-14 15:56:25 -0300

    fix(gsf): validate changes before run

--- git-mnz.plugin.zsh
+++ git-mnz.plugin.zsh
@@ -1,5 +1,5 @@
 #!zsh
-local version='1.6.0'
+local version='1.6.1'
 local GIT_MNZ=$ZSH_CUSTOM/plugins/git-mnz
 
 alias gmnzv='echo "git-mnz v.${version}"'
@@ -232,7 +232,7 @@ function _gsf() {
   done
 
   if [ "$branch" != "" ]; then
-    grh --soft "$(git merge-base --fork-point $branch)" && gc
+    grb HEAD && grh --soft "$(git merge-base --fork-point $branch)" && gc
   fi
 }
 
